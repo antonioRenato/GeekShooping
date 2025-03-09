df874c1627dcc69ed8373f94b156e7c4de1f3f28
antonioRenato <antoniorpfs@gmail.com>
2025-03-09 19:26:40 -0300

    Add project files.

---
+++ Services/GeekShooping.ProductAPI/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using GeekShooping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShooping.ProductAPI.Model
+{
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+    }
+}
