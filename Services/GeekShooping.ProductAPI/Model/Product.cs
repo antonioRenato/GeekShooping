4396d894908abd1582af4d286b993a9fee93da8f
antonioRenato <antoniorpfs@gmail.com>
2025-03-09 20:19:04 -0300

    Create migrations and created a new project asp net core web mvc

--- Services/GeekShooping.ProductAPI/Model/Product.cs
+++ Services/GeekShooping.ProductAPI/Model/Product.cs
@@ -16,5 +16,19 @@ namespace GeekShooping.ProductAPI.Model
         [Required]
         [Range(1, 10000)]
         public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(150)]
+        public string ImageUrl { get; set; }
+
+
     }
 }
