3c9c0cd297ce2f6f023c47dfb0e5e5582e1c990f
antonioRenato <antoniorpfs@gmail.com>
2025-03-12 05:44:59 -0300

    Created controller and method find and findById

---
+++ Services/GeekShooping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using GeekShooping.ProductAPI.Data.ValueObjects;
+using GeekShooping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var product = await _repository.FindAll();
+
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+
+            return product == null ? NotFound() : Ok(product);
+        }
+    }
+}
