e81b9ced5f415997361f385d94cba22afd73a63f
antonioRenato <antoniorpfs@gmail.com>
2025-03-18 21:44:28 -0300

    Created Identity server on program.cs

--- Services/GeekShopping.IdentityServer/Program.cs
+++ Services/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,34 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static GeekShopping.IdentityServer.Model.Context.MySqlContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 41))));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().
+    AddEntityFrameworkStores<MySQLContext>().
+    AddDefaultTokenProviders();
+
+var build = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(
+    IdentityConfiguration.IdentityResources).
+    AddInMemoryClients(IdentityConfiguration.Clients).
+    AddAspNetIdentity<ApplicationUser>();
+
+build.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -13,7 +40,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
